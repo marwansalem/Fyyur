21167f257175e36585f5f1b5e1300c7fd7ba4058
Marwan Saad <marwan10salem@hotmail.com>
2020-11-23 15:22:30 +0200

    connected to postgres

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -8,10 +8,12 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+import config
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -19,9 +21,16 @@ from forms import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
+
+migrate = Migrate(app, db)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = app.config.getSQLALCHEMY_DATABASE_URI
+
+#print(app.config.SQLALCHEMY_DATABASE_URI)
 
 #----------------------------------------------------------------------------#
 # Models.
@@ -56,6 +65,9 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+  __tablename= 'Show'
 
 #----------------------------------------------------------------------------#
 # Filters.
--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,8 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+## connection url with username as postgres and password 1234
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/fyyur' 
+#'<Put your local database url>'
+
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/fyyur'

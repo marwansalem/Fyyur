dbbbd7d18efec1e472aeb56149e6fe395fd4c6c2
Marwan Saad <marwan10salem@hotmail.com>
2020-11-29 20:17:05 +0200

    use env variables in config updated requirements.txt

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -11,7 +11,11 @@ DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
 ## connection url with username as postgres and password 1234
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/fyyur' 
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'fyyur') 
+DB_PASSWORD = os.getenv('DB_PASSWORD', '1234') 
+
+
+SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}' 
 #'<Put your local database url>'
 
-##app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/fyyur'

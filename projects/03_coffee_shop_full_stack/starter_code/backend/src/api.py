849445856573c36ad3873e8aac25e9077a8d52d9
Marwan Saad <marwan10salem@hotmail.com>
2021-01-04 23:59:05 +0200

    implement endpoints

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -19,36 +19,73 @@ CORS(app)
 # db_drop_and_create_all()
 
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    '''
+    @TODO implement endpoint
+        GET /drinks
+            it should be a public endpoint
+            it should contain only the drink.short() data representation
+        returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+            or appropriate status code indicating reason for failure
+    '''
+    try:
+        drinks = [drink.short() for drink in Drink.query.all()]
+    except:
+        abort(500)
+
+    return 200, jsonify({
+        'success': True,
+        'drinks': drinks
+    })
+
+@app.route('/drinks-details', methods=['GET'])
+def get_drink_details():
+    '''
+    @TODO implement endpoint
+        GET /drinks-detail
+            it should require the 'get:drinks-detail' permission
+            it should contain the drink.long() data representation
+        returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+            or appropriate status code indicating reason for failure
+    '''
+    drinks = [drink.long() for drink in Drink.query.all()]
+    return 200, jsonify({
+        'success': True,
+        'drinks':drinks
+    }) 
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks', methods=['POST'])
+def post_drinks():
+    '''
+    @TODO implement endpoint
+        POST /drinks
+            it should create a new row in the drinks table
+            it should require the 'post:drinks' permission
+            it should contain the drink.long() data representation
+        returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+            or appropriate status code indicating reason for failure
+    '''
+    body = request.get_json()
+    try:
+        title = body['title']
+        recipe = body['recipe']
+        new_drink = Drink(title=title, recipe=recipe)
+    except:
+        abort(400)
 
+    try:
+        new_drink.insert()
+    except:
+        abort(500)
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
-
+    return 200, jsonify({
+        'success':True,
+        'drinks': [new_drink.long()]
+    })
+    
 
 '''
 @TODO implement endpoint
@@ -63,17 +100,31 @@ CORS(app)
 '''
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<drink_id>', methods=['DELETE'])
+def delete_drink(drink_id):
+    '''
+    @TODO implement endpoint
+        DELETE /drinks/<id>
+            where <id> is the existing model id
+            it should respond with a 404 error if <id> is not found
+            it should delete the corresponding row for <id>
+            it should require the 'delete:drinks' permission
+        returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+            or appropriate status code indicating reason for failure
+    '''
+    drink = Drink.query.get(drink_id)
+    if drink is None:
+        abort(404)
+    
+    try:
+        drink.delete()
+    except:
+        abort(500)
 
+    return 200, jsonify({
+        'success':True,
+        'delete': drink_id
+    })
 
 ## Error Handling
 '''
